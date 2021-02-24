115f2136fa979837f852b9573f19b88ac01e1716
Ramon Duraes <ramongduraes@gmail.com>
2021-02-24 20:21:33 -0300

    adding docstring

--- stringanalysis/stringanalysis.py
+++ stringanalysis/stringanalysis.py
@@ -7,6 +7,12 @@ class StringAnalysis:
         self.string_stream = string_stream
 
     def get_most_similar_word(self, word):
+        """
+        Returns the most similar word on the corpus as measured by the Jaro distance.
+
+        :param word: desired query word.
+        :return: the most similar word on the corpus (defined on the class constructor).
+        """
         scores = list(map(lambda x: jellyfish.jaro_distance(x, word),
                           self.string_stream))
         max_score = max(zip(scores, range(len(scores))))[1]
